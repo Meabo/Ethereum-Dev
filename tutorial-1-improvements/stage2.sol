7d2604b0f80c1e3d2c2980b893831a5e492b99a2
Mehdi Abou <aboumehdi.pro@gmail.com>
2018-10-13 17:38:30 +0200

    Pushing improvements

--- tutorial-1-improvements/stage2.sol
+++ tutorial-1-improvements/stage2.sol
@@ -4,14 +4,37 @@ contract BLT
 {
     mapping(address => uint256) public balanceOf;
 
-    constructor(uint256 initsupply) public
+    /* Improvements Stage 1*/
+    string public version = "BLT v1.0";
+    string public name_;
+    string public symbol_;
+    uint8 public decimal_;
+    uint256 public totalSupply_;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+
+    constructor(uint256 initsupply, string name, string symbol, uint8 decimal) public
     {
         balanceOf[msg.sender] = initsupply;
+        name_ = name;
+        symbol_ = symbol;
+        decimal_ = decimal;
+        totalSupply_ = initsupply;
     }
 
-    function transfer(address to_, uint256 _value) public
+    function transfer(address to, uint256 value) public
     {
-        balanceOf[msg.sender] -= _value;
-        balanceOf[to_] += _value;
+        // We check if we have a sufficient amount to send
+        require(balanceOf[msg.sender] > value);
+
+        // Overflow check (example 255 + 10 : is an overflow)
+        require(balanceOf[to] + value > balanceOf[to]);
+
+        emit Transfer(msg.sender, to, value);
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
     }
 }
